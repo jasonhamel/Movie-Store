c6ca8f38e2d8b6380f89c0d09ee06c53f6b3290f
Jason <github@jasongetsemail.com>
2024-10-20 13:20:44 -0400

    Add the mongo package using Maven, build the QuickStart.java Class to test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,37 +1,9 @@
 package main;
 
-import java.sql.*;
-import java.util.concurrent.TimeUnit;
-
 
 public class Main {
     public static void main(String[] args) {
+        String uri = "mongodb+srv://jason:W98ZYGXvqjqBiQLI@cluster0.ijrik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-        System.out.println("Hello world!");
-//        HDDVD movie = new HDDVD("Flatliners", 99.99, 120, false, "R", 1990);
-//        System.out.println(movie);
-//        System.out.println(movie.getName());
-//        System.out.println(movie.getCost());
-//        System.out.println(movie.getRunTime());
-//        System.out.println(movie.getStarsNickCage());
-//        System.out.println(movie.getRating());
-//        System.out.println(movie.getYearOfRelease());
-//        System.out.println();
-//        movie.setName("Jason");
-//        movie.setStarsNickCage(true);
-//        movie.updateCost(50);
-//        movie.setRunTime(999);
-//        movie.setRating("PG13");
-//        movie.setYearOfRelease(1987);
-//        System.out.println(movie);
-//        System.out.println(movie.getName());
-//        System.out.println(movie.getCost());
-//        System.out.println(movie.getRunTime());
-//        System.out.println(movie.getStarsNickCage());
-//        System.out.println(movie.getRating());
-//        System.out.println(movie.getYearOfRelease());
-//        Store.getInstance().changeStock(movie, 7);
-//        //Store.getInstance().changeStock("Die Hard 2", 10);
-//        Store.getInstance().changeStock(movie, 1);
     }
 }
