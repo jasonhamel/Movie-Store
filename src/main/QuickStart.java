69d8a89b9e36577448f73c7440ab8e0fc14bcbe3
Jason <github@jasongetsemail.com>
2024-11-03 15:19:37 -0500

    Add logic to get a list of 10 movies loaded into the database if there are no movies in the database

--- src/main/QuickStart.java
+++ src/main/QuickStart.java
@@ -1,6 +1,8 @@
 package main;
 
 import main.model.movie.Bluray;
+import main.model.movie.DVD;
+import main.model.movie.HDDVD;
 import main.model.movie.Movie;
 import org.bson.Document;
 import com.mongodb.client.MongoClient;
@@ -29,37 +31,99 @@ public class QuickStart {
 
             } else {
                 System.out.println("No record. Creating now.");
-                ArrayList<Bluray> blurays = new ArrayList<>();
-                ArrayList<Movie> movie = scanFile();
-                blurays.add(new Bluray(movie.get(0).getName(), movie.get(0).getCost(), movie.get(0).getRunTime(),
-                        movie.get(0).getStarsNickCage(), movie.get(0).getRating(), movie.get(0).getYearOfRelease()));
+                ArrayList<HDDVD> hddvds = createHDDVDs();
+                ArrayList<Bluray> blurays = createBlurays();
+                ArrayList<DVD> dvds = createDVDs();
 
-                Document movieToAdd = new Document("Name", blurays.get(0).getName());
+                for (HDDVD hddvd : hddvds) {
+                    Document movieToAdd = new Document("Name", hddvd.getName());
 
-                movieToAdd.append("Cost", blurays.get(0).getCost());
-                movieToAdd.append("Run Time", blurays.get(0).getRunTime());
-                movieToAdd.append("Stars Nick Cage", blurays.get(0).getStarsNickCage());
-                movieToAdd.append("Rating", blurays.get(0).getRating());
-                movieToAdd.append("Year of Release", blurays.get(0).getYearOfRelease());
-                movieToAdd.append("Type ", blurays.get(0).getClass().getSimpleName());
-                collection.insertOne(movieToAdd);
+                    movieToAdd.append("Cost", hddvd.getCost());
+                    movieToAdd.append("Run Time", hddvd.getRunTime());
+                    movieToAdd.append("Stars Nick Cage", hddvd.getStarsNickCage());
+                    movieToAdd.append("Rating", hddvd.getRating());
+                    movieToAdd.append("Year of Release", hddvd.getYearOfRelease());
+                    movieToAdd.append("Type ", hddvd.getClass().getSimpleName());
+                    collection.insertOne(movieToAdd);
+                }
+
+                for (Bluray bluray : blurays) {
+                    Document movieToAdd = new Document("Name", bluray.getName());
+
+                    movieToAdd.append("Cost", bluray.getCost());
+                    movieToAdd.append("Run Time", bluray.getRunTime());
+                    movieToAdd.append("Stars Nick Cage", bluray.getStarsNickCage());
+                    movieToAdd.append("Rating", bluray.getRating());
+                    movieToAdd.append("Year of Release", bluray.getYearOfRelease());
+                    movieToAdd.append("Type ", bluray.getClass().getSimpleName());
+                    collection.insertOne(movieToAdd);
+                }
+
+                for (DVD dvd : dvds) {
+                    Document movieToAdd = new Document("Name", dvd.getName());
+
+                    movieToAdd.append("Cost", dvd.getCost());
+                    movieToAdd.append("Run Time", dvd.getRunTime());
+                    movieToAdd.append("Stars Nick Cage", dvd.getStarsNickCage());
+                    movieToAdd.append("Rating", dvd.getRating());
+                    movieToAdd.append("Year of Release", dvd.getYearOfRelease());
+                    movieToAdd.append("Type ", dvd.getClass().getSimpleName());
+                    collection.insertOne(movieToAdd);
+                }
+
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public static ArrayList<HDDVD> createHDDVDs() throws FileNotFoundException {
+        ArrayList<HDDVD> hddvds = new ArrayList<>();
+        ArrayList<Movie> movies = scanFile();
+
+        for (int i = 0; i < movies.size(); i++) {
+            hddvds.add(new HDDVD(movies.get(i).getName(), movies.get(i).getCost(), movies.get(i).getRunTime(),
+                    movies.get(i).getStarsNickCage(), movies.get(i).getRating(), movies.get(i).getYearOfRelease()));
+        }
+        return hddvds;
+    }
+
+    public static ArrayList<Bluray> createBlurays() throws FileNotFoundException {
+        ArrayList<Bluray> blurays = new ArrayList<>();
+        ArrayList<Movie> movies = scanFile();
+
+        for (int i = 0; i < movies.size(); i++) {
+            blurays.add(new Bluray(movies.get(i).getName(), movies.get(i).getCost(), movies.get(i).getRunTime(),
+                    movies.get(i).getStarsNickCage(), movies.get(i).getRating(), movies.get(i).getYearOfRelease()));
+        }
+        return blurays;
+    }
+
+    public static ArrayList<DVD> createDVDs() throws FileNotFoundException {
+        ArrayList<DVD> dvds = new ArrayList<>();
+        ArrayList<Movie> movies = scanFile();
+
+        for (int i = 0; i < movies.size(); i++) {
+            dvds.add(new DVD(movies.get(i).getName(), movies.get(i).getCost(), movies.get(i).getRunTime(),
+                    movies.get(i).getStarsNickCage(), movies.get(i).getRating(), movies.get(i).getYearOfRelease()));
+        }
+        return dvds;
+    }
+
     public static ArrayList<Movie> scanFile() throws FileNotFoundException {
         ArrayList<Movie> movieToReturn = new ArrayList<>();
         FileInputStream fis = new FileInputStream("movie-list.txt");
         Scanner scan = new Scanner(fis).useDelimiter(",");
-        String name = scan.next();
-        double cost = scan.nextDouble();
-        double runTime = scan.nextDouble();
-        boolean starsNickCage = scan.nextBoolean();
-        String rating = scan.next();
-        int yearOfRelease = scan.nextInt();
-        movieToReturn.add(new Movie(name, cost, runTime, starsNickCage, rating, yearOfRelease));
+        while (scan.hasNext()) {
+            String name = scan.next();
+            double cost = scan.nextDouble();
+            double runTime = scan.nextDouble();
+            boolean starsNickCage = scan.nextBoolean();
+            String rating = scan.next();
+            int yearOfRelease = scan.nextInt();
+            movieToReturn.add(new Movie(name, cost, runTime, starsNickCage, rating, yearOfRelease));
+        }
+
 
         return movieToReturn;
     }
