860885f0cb9e244b3d7c4a5891d3171cddc51784
Jason <github@jasongetsemail.com>
2024-11-06 13:36:54 -0500

    Move from the QuickStart.java class to the DataConnection.java class.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        String uri = "mongodb+srv://jason:W98ZYGXvqjqBiQLI@cluster0.ijrik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    public static void main(String[] args) throws FileNotFoundException {
+
+        DataConnection.main(args);
 
     }
 }
