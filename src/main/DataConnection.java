717173278d07374356c21802e41acf8c8e37ffea
Jason <github@jasongetsemail.com>
2024-12-31 15:52:39 -0500

    Convert DataConnection.java to a Singleton. Create FileLoader.java to handle the reading from file logic, and to load the movies using the Singleton

--- src/main/DataConnection.java
+++ src/main/DataConnection.java
@@ -2,86 +2,35 @@ package main;
 
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
-import main.model.movie.Bluray;
 import main.model.movie.DVD;
-import main.model.movie.HDDVD;
-import main.model.movie.Movie;
 import org.bson.Document;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-
-import static com.mongodb.client.model.Filters.eq;
 
 public class DataConnection {
-    public static void main( String[] args ) {
+    private static DataConnection instance;
+    MongoCollection<Document> collection;
+
+    private DataConnection() {
         final String DB_KEY = System.getenv("DB_KEY");
         String uri = "mongodb+srv://jason:" + DB_KEY + "@cluster0.ijrik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-        MongoCollection<Document> collection;
 
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("Movie_Store");
-            collection = database.getCollection("movies");
-            Document doc = collection.find(eq("Name", "Hot Rod")).first();
-            if (doc != null) {
-                System.out.println("Emptying database");
-                collection.drop();
-            }
-            System.out.println("No record. Creating now.");
-            ArrayList<Movie> moviesFromFile = scanFile();
-            ArrayList<Document> moviesToAdd = new ArrayList<>();
+        MongoClient mongoClient = MongoClients.create(uri);
+        MongoDatabase database = mongoClient.getDatabase("Movie_Store");
+        collection = database.getCollection("movies");
+    }
 
-            for (Movie movie: moviesFromFile) {
-                moviesToAdd.add(getDocument(new HDDVD(movie)));
-                moviesToAdd.add(getDocument(new Bluray(movie)));
-                moviesToAdd.add(getDocument(new DVD(movie)));
-            }
-
-            collection.insertMany(moviesToAdd);
-
-
-        } catch (
-                FileNotFoundException e) {
-            throw new RuntimeException(e);
+    public static synchronized DataConnection getInstance() {
+        if (instance == null) {
+            instance = new DataConnection();
         }
+        return instance;
     }
 
-    private static Document getDocument(Movie movie) {
-        Document movieToAdd = new Document("Name", movie.getName());
 
-        movieToAdd.append("Cost", movie.getCost());
-        movieToAdd.append("Run Time", movie.getRunTime());
-        movieToAdd.append("Stars Nick Cage", movie.getStarsNickCage());
-        movieToAdd.append("Rating", movie.getRating());
-        movieToAdd.append("Year of Release", movie.getYearOfRelease());
-        movieToAdd.append("Type", movie.getClass().getSimpleName());
-        return movieToAdd;
-    }
-
-    private static ArrayList<Movie> scanFile() throws FileNotFoundException {
-        ArrayList<Movie> movieToReturn = new ArrayList<>();
-        FileInputStream fis = new FileInputStream("movie-list.txt");
-        Scanner scan = new Scanner(fis).useDelimiter(",");
-        while (scan.hasNext()) {
-            String name = scan.next();
-            double cost = scan.nextDouble();
-            double runTime = scan.nextDouble();
-            boolean starsNickCage = scan.nextBoolean();
-            String rating = scan.next();
-            int yearOfRelease = scan.nextInt();
-            movieToReturn.add(new Movie(name, cost, runTime, starsNickCage, rating, yearOfRelease));
-        }
-
-        scan.close();
-        return movieToReturn;
-    }
 
     public static void checkForDVD(String title) {
        DVD foundDVD;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,11 @@
 package main;
 
-
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-
-        DataConnection.main(args);
-        DataConnection.checkForDVD("Hot Rod");
-
+        DataConnection.getInstance();
+        DataConnection.getInstance().collection.drop();
+        FileLoader.loadFiles();
     }
 }
