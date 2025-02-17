f475674e94a592804054a7dff7b5e6aa8c986175
Jason <github@jasongetsemail.com>
2025-02-16 20:55:50 -0500

    Add Hello World and commend out database connection

---
+++ src/main/java/com/JasonVideo/Movie_Store/MovieStoreApplication.java
@@ -0,0 +1,14 @@
+package com.JasonVideo.Movie_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStoreApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(MovieStoreApplication.class, args);
+		System.out.println("Hello, World");
+	}
+
+}
