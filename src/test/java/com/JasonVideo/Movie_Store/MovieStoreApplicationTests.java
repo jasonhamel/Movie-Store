41b7df976cc2e7befb8482112e6a8f71099a9b75
Jason <github@jasongetsemail.com>
2025-02-17 11:35:48 -0500

    Add Test class to git

---
+++ src/test/java/com/JasonVideo/Movie_Store/MovieStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JasonVideo.Movie_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
